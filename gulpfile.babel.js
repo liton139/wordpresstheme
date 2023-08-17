515f63ce210fa470ba141a55317c5d765115f8ed
liton139 <52035063+liton139@users.noreply.github.com>
2023-08-18 01:48:16 +0600

    gulp clean and del command last update

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,25 +3,61 @@ import yargs from 'yargs';
 //import sass from 'gulp-sass';
 import cleanCSS from 'gulp-clean-css';
 import gulpif from 'gulp-if';
-
+import sourcemaps from 'gulp-sourcemaps';
+import imagemin from 'gulp-imagemin';
+//import del from 'del';
 
 
 const PRODUCTION =yargs.argv.prod;
-const sass = require('gulp-sass')(require('sass'));
+const sass = require('gulp-sass') (require('sass'));
+const del = require('del');
+const paths = {
+    styles:{
+        src: ['src/assets/scss/bundle.scss','src/assets/scss/admin.scss'],
+        dest: 'dist/assets/css'
+    },
+    images: {
+        src: 'src/assets/images/**/*.{jpg,jpeg,png,svg,gif}',
+        dest: 'dist/assets/images'
+    },
+    others: {
+        src: ['src/assets/**/*', '!src/assets/{images,js,scss}', '!src/assets/{images,js,scss}/**/*'],
+        dest: 'dist/assets'
+    }
+}
+
+//export const clean = () => del(['dist']);
+export const clean = () => { return del('dist/**', { force: true });
+}
 
 export const styles = () => {
-    return gulp.src('src/assets/scss/bundle.scss')
+    return gulp.src(paths.styles.src)
+        .pipe(gulpif(!PRODUCTION, sourcemaps.init()))
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulpif(PRODUCTION,cleanCSS({compatibility: 'ie8'})))
-        .pipe(gulp.dest('dist/assets/css'));
+        .pipe(gulpif(PRODUCTION, cleanCSS({compatibility: 'ie8'})))
+        .pipe(gulpif(!PRODUCTION, sourcemaps.write()))
+        .pipe(gulp.dest(paths.styles.dest));
 
 }
-//export default hello;
 
-
-//fresh code is here 
-// export const hello = (done) => {
-//     console.log(PRODUCTION);
-//     done();
+export const images = () => {
+    return gulp.src(paths.images.src)
+    .pipe(gulpif(!PRODUCTION, imagemin()))
+    .pipe(gulp.dest(paths.images.dest));
+}
+// export const images = () => {
+//     return import('gulp-imagemin').then(imagemin => {
+//         return gulp.src(paths.images.src)
+//             .pipe(gulpif(!PRODUCTION, imagemin.default()))
+//             .pipe(gulp.dest(paths.images.dest));
+//     });
 // }
-// export default hello;
+
+export const watch = () => {
+    gulp.watch('src/assets/scss/**/*.scss', styles);
+}
+
+export const copy = () => {
+    return gulp.src(paths.others.src)
+        .pipe(gulp.dest(paths.others.dest));
+}
--- lib/enqueue-assets.php
+++ lib/enqueue-assets.php
@@ -6,3 +6,10 @@ function mytheme_assets(){
 }
 
 add_action ('wp_enqueue_scripts', 'mytheme_assets');
+
+function mytheme_admin_assets(){
+    wp_enqueue_style('mytheme-admin-stylesheet', get_template_directory_uri() . 
+    '/dist/assets/css/admin.css', array(), '1.0.0','all');
+}
+
+add_action ('admin_enqueue_scripts', 'mytheme_assets');
