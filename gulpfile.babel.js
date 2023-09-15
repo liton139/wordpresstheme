e7c1d71065429410fcd5aed001ed979a0398adda
liton139 <52035063+liton139@users.noreply.github.com>
2023-09-15 20:50:42 +0600

    complete theme run with plugin

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -124,8 +124,11 @@ export const scripts = () => {
 
 export const compress = () => {
     return gulp.src(paths.package.src)
-        .pipe(replace('_themename', info.name))
-        .pipe(zip(`${info.name}.zip`))
+        .pipe(gulpif(
+            (file) => (file.relative.split('.').pop() !== 'zip'),
+            replace('_themename', info.name)))
+        .pipe(zip(`${info.name}.zip`)
+        )       
         .pipe(gulp.dest(paths.package.dest));
 }
 
--- lib/helpers.php
+++ lib/helpers.php
@@ -2,18 +2,17 @@
 
 if(!function_exists('_themename_post_meta')){
 function _themename_post_meta(){
-    /* translators: %s: Post Date */ 
-   printf(
-    esc_html__( 'Posted on %s', '_themename' ),
-    '<a href="' . esc_url(get_permalink( )) . '"> <time datetime="' . esc_attr(get_the_date( 'c')) .'">'. esc_html(get_the_time()) . '</time></a>'
-   );
-    /* translators: %s: Post Author */ 
-   printf(
-     esc_html__(' By %s', '_themename'),
-     '<a href="' . esc_url( get_author_posts_url( get_the_author_meta('ID' ) )). '">' . esc_html(get_the_author()). '</a>'
+/* translators: %s: Post Date */ 
+printf(
+esc_html__( 'Posted on %s', '_themename' ),
+'<a href="' . esc_url(get_permalink( )) . '"> <time datetime="' . esc_attr(get_the_date( 'c')) .'">'. esc_html(get_the_time()) . '</time></a>'
+);
+/* translators: %s: Post Author */ 
+printf(
+esc_html__(' By %s', '_themename'),
+'<a href="' . esc_url( get_author_posts_url( get_the_author_meta('ID' ) )). '">' . esc_html(get_the_author()). '</a>'
 
-   );  
-   
+);   
 }}
 
 function _themename_readmore_link(){
