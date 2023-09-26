d597bb44f1aa19a0a9f807b7c81cec1e9aca2d2a
liton139 <52035063+liton139@users.noreply.github.com>
2023-09-26 23:38:48 +0600

    Creating The Single Blog Post Template

--- src/assets/js/customize-preview.js
+++ src/assets/js/customize-preview.js
@@ -7,6 +7,15 @@ wp.customize('blogname', (value) => {
         $('.c-header__blogname').html(to);
     })
 })
+wp.customize('_themename_display_author_info', (value) => {
+    value.bind( (to) => {
+        if(to){
+            $('.c-post-author').show();
+        } else {
+            $('.c-post-author').hide();
+        }
+    })
+})
 wp.customize('_themename_accent_colour', (value) => {
     value.bind( (to) => {
         let inline_css = ``;
